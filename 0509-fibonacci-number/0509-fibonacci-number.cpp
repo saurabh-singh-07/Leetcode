56503abf1dba1a9829d927d5f3120ed532217da7
Saurabh singh <ajinkyabisht75@gmail.com>
2026-07-05 20:58:32 +0530

    Time: 3 ms (65.74%), Space: 7.8 MB (64.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
+        if(n <=  1) return n;
         int a = 0; 
         int b = 1;
-        int i = 0;
+        int i = 1;
         while(i < n){
             int c = a + b;
             a = b;
             b = c;
             i ++;
         }
-        return a;
+        return b;
     }
 };
