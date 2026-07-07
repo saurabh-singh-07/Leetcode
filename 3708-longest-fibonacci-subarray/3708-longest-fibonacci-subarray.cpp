9d7845fc7ba034a7cb037e1a60e17f9b46af2ea6
Saurabh singh <ajinkyabisht75@gmail.com>
2026-07-07 09:22:17 +0530

    Time: 11 ms (6.2%), Space: 102.7 MB (42.43%) - LeetHub

--- 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.cpp
+++ 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int n = nums.size();
-        if(n <= 2) return n;
+        if(nums.size() <= 2) return nums.size();
 
         int ans = 2;
         int i = 0;
         
-        while(i < n-1){
+        while(i < nums.size()-1){
             int prev = nums[i];
             int curr = nums[i+1];
             int len = 2;
             int next = i+2;
 
-            while(next < n && nums[next] == prev+curr){
+            while(next < nums.size() && nums[next] == prev+curr){
                 len++;
                 prev = curr;
                 curr=nums[next];
