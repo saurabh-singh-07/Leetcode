2dca76bd6e7f450844f3f064ffba4427b48fa096
Saurabh singh <ajinkyabisht75@gmail.com>
2026-07-14 09:46:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (54.9%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0;
+        int j = s.size() -1;
+        while(i < j){
+            if(!isalpha(s[i]))
+                i++;
+            else if(!isalpha(s[j]))
+                j--;
+
+            if(isalpha(s[i]) && isalpha(s[j])){
+                swap(s[i], s[j]);
+                i ++;
+                j --;
+            }
+        }
+        return s;
+    }
+};
