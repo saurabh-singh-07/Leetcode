7a47525ba020e5ffb0f9b7bc7eff2c55d159592c
Saurabh singh <ajinkyabisht75@gmail.com>
2026-07-28 10:06:31 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        unordered_map<char,int> mp;
+        for(char c : s){
+            mp[c]++;
+        }
+
+        for(char c : t){
+            if(mp.find(c) == mp.end() || mp[c] == 0 ){
+                return false;
+            }
+            mp[c]--;
+        }
+
+        return true;
+    }
+};
