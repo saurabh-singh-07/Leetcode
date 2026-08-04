bad82320a86801714e25e25f571bd46bf2f1325c
Saurabh singh <ajinkyabisht75@gmail.com>
2026-08-04 12:01:44 +0530

    Time: 0 ms (100%), Space: 15.1 MB (27.08%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,26 +11,40 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head == nullptr ) return head;
-        
+        // if(head == nullptr) return head;
+        // int count = 1;
+        // ListNode * curr = head; 
+        // while(curr-> next) {
+        //     curr = curr-> next;
+        //     count ++;
+        // }
+
+        // if(n > count) return head;
+
+        // if(n == count){
+        //     ListNode * del = head;
+        //     head = head-> next;
+        //     delete  del;
+        //     return head;
+        // }
+
+        // count = count - n;
+        // curr = head;
+        // for(int i = 1; i < count; i ++){
+        //     curr = curr -> next;
+        // }
+        // curr -> next = curr-> next -> next;
+        // return head;
+
+
+        ListNode * ptr = head;
         ListNode * temp = head;
 
-        int count = 1;
-        while(temp-> next != nullptr){
-            temp = temp -> next;
-            count ++;
-        }
-        if(count < n) return head;
-        if(count == n){
-            ListNode * del = head;
-            head = head -> next;
-            delete del;
-            return head;
-        }
-        count = count - n;
-        temp = head;
-
-        for(int i = 1; i < count; i ++){
+        for(int i = 0; i < n; i ++) ptr = ptr -> next;
+        
+        if(!ptr) return head-> next;
+        while(ptr-> next){
+            ptr= ptr-> next;
             temp = temp -> next;
         }
         temp -> next = temp -> next -> next;
