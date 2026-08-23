74262f55548c2c68cef99eceecd196fc9bc48496
Saurabh singh <ajinkyabisht75@gmail.com>
2026-08-23 21:10:01 +0530

    Time: 584 ms (8.57%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -5,7 +5,11 @@
 -- ON P1.email = P2.email AND P1.id <> P2.id;
 
 
-SELECT DISTINCT (P1.Email)
-FROM Person P1
-INNER JOIN Person P2
-WHERE P1.email = P2.email AND P1.id <> P2.id;
+-- SELECT DISTINCT (P1.Email)
+-- FROM Person P1
+-- INNER JOIN Person P2
+-- WHERE P1.email = P2.email AND P1.id <> P2.id;
+
+SELECT Email FROM Person
+GROUP BY Email
+HAVING COUNT(email) > 1;
