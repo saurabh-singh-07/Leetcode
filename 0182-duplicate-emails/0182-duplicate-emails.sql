5b62594e695465c726263b0ee680a12c47a48bb9
Saurabh singh <ajinkyabisht75@gmail.com>
2026-08-23 21:08:30 +0530

    Time: 458 ms (26.85%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
+-- SELECT DISTINCT (P1.Email)
+-- FROM Person P1
+-- INNER JOIN Person P2
+-- ON P1.email = P2.email AND P1.id <> P2.id;
+
+
 SELECT DISTINCT (P1.Email)
 FROM Person P1
 INNER JOIN Person P2
-ON P1.email = P2.email AND P1.id <> P2.id;
+WHERE P1.email = P2.email AND P1.id <> P2.id;
